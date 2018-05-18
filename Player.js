aed99c9cc47ea42427cc94916b5e6288365bc99c
Andrew Budziszek <andrew@budziszek.com>
2018-05-17 21:19:25 -0500

    I beat level 3!

--- Player.js
+++ Player.js
@@ -1,7 +1,13 @@
 class Player {
   playTurn(warrior) {
     // Cool code goes here.
-    if (warrior.feel().isUnit()) {
+    if (warrior.health() < 12) {
+      if (warrior.feel().isUnit()) {
+        warrior.walk('backward');
+      } else {
+        warrior.rest();
+      }
+    } else if (warrior.feel().isUnit()) {
       warrior.attack();
     } else {
       warrior.walk();
